c66c01bd55316ff96df17331fd6ff56bb51c66e6
Vanessa Yévenes Reyes <vyevenes@Vanessas-MacBook.local>
2015-11-08 23:27:35 -0800

    Add styles and some comments in the code

--- app.js
+++ app.js
@@ -6,21 +6,36 @@ var addItem = function() {
     alert('Please write a item');
     return;
   }
+  // The function createElement get two arguments (element and text) >> la funcion createElement recibe 2 argumentos (elemento, texto)
+  // The element and text are a string  >> el elemento es un string y texto es un string
+  function createElement(element, text) {
+    element = document.createElement(element);
+    text = document.createTextNode(text);
+    element.appendChild(text);
+    return element;
+  }
 
-  // crear element
-  var li = document.createElement("li");
-  var span = document.createElement("span");
-  var spanText = document.createTextNode("Remove");
-  var text = document.createTextNode(value);
-  var ul = document.getElementById("list");
-  span.appendChild(spanText);
-  li.appendChild(text);
-  li.appendChild(span);
+  // create element
+  var li = createElement('li', value);
+  var removeButton = createElement('span', 'Remove');
+
+  // Get the element with the id list >> obtiene el elemento con el id list
+  var ul = document.getElementById('list');
+
+  // Add >> agrega el remove button al elemento li que vamos a agregar a la lista
+  li.appendChild(removeButton);
+
+  // bindea el elemento al btn remover //url: http://www.w3schools.com/jquery/event_bind.asp
   bindClickToRemoveButton(li);
+
+  // Al elemento ul le inserta un li
   ul.appendChild(li);
+
+  // reset the input cada vez que inserta un li
   clearInput();
 };
 
+// url http://stackoverflow.com/questions/6750445/javascript-to-clear-form-field-on-button-click
 var clearInput = function() {
   document.getElementById("input").value = '';
 }
